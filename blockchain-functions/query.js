bb563e74b94266894e83fd855cf2f6ff7f026a27
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2019-09-14 14:40:00 -0500

    Rewards functions

--- blockchain-functions/query.js
+++ blockchain-functions/query.js
@@ -73,7 +73,73 @@ async function getHistoryForBloodBag(id,user) {
     }
 }
 
+async function getRewardsHistory(userId,user) {
+    try {
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/blockchain-functions/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(user);
+        if (!userExists) {
+            console.log('An identity for the user '+user+' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('rewards');
+        // Get the result and send it back
+        const result = await contract.evaluateTransaction('getTransactionHistory',userId);
+        return JSON.parse(result)
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        return error
+    }
+}
+
+async function readWallet(userId,user) {
+    try {
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), '/blockchain-functions/wallet');
+        const wallet = new FileSystemWallet(walletPath);
+
+        // Check to see if we've already enrolled the user.
+        const userExists = await wallet.exists(user);
+        if (!userExists) {
+            console.log('An identity for the user '+user+' does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccpPath, { wallet, identity: user, discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('rewards');
+        // Get the result and send it back
+        const result = await contract.evaluateTransaction('readUser',userId);
+        return JSON.parse(result)
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        return error
+    }
+}
+
 module.exports = {
     readBloodBag,
-    getHistoryForBloodBag
+    getHistoryForBloodBag,
+    getRewardsHistory,
+    readWallet
 }
--- index.js
+++ index.js
@@ -9,132 +9,202 @@
  * Built on: September 12th, 2019                                                                                                        
  */
 "use-strict"
- const express = require('express')
- const asyncHandler = require('express-async-handler')
- const query = require('./blockchain-functions/query')
- const invoke = require('./blockchain-functions/invoke')
- const enroll = require('./blockchain-functions/enrollAdmin')
- const user = require('./blockchain-functions/registerUser')
- const app = express()
- const port = 80
+const express = require('express')
+const asyncHandler = require('express-async-handler')
+const query = require('./blockchain-functions/query')
+const invoke = require('./blockchain-functions/invoke')
+const enroll = require('./blockchain-functions/enrollAdmin')
+const user = require('./blockchain-functions/registerUser')
+const app = express()
+const port = 80
 
- app.use(express.json())
+app.use(express.json())
 
- /**
-  * Online Set-up
-  */
- app.post('/enroll',asyncHandler(async (req,res,next) => {
+/**
+ * Online Set-up
+ */
+app.post('/enroll', asyncHandler(async (req, res, next) => {
     const enan = await enroll.main()
-	 const origin = request.get('origin');
+    const origin = request.get('origin');
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
     res.send(enan)
- }))
+}))
 
- app.post('/user',asyncHandler(async (req,res,next) => {
-     const unan = await user.main()
-	 const origin = request.get('origin');
-	 res.header('Access-Control-Allow-Origin', origin);
+app.post('/user', asyncHandler(async (req, res, next) => {
+    const unan = await user.main()
+    const origin = request.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-     res.send(unan)
- }))
+    res.send(unan)
+}))
 
- app.post('/test',asyncHandler(async (req,res,next) => {
-     const result = await invoke.testInvoke('user1')
-	 const origin = request.get('origin');
-	 res.header('Access-Control-Allow-Origin', origin);
+app.post('/test', asyncHandler(async (req, res, next) => {
+    const result = await invoke.testInvoke('user1')
+    const origin = request.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-     res.send(result)
- }))
+    res.send(result)
+}))
 
- /**
-  * Creates a new blood bag
-  */
- app.post('/blood/create/:bagId/:bagOriginID/:bagLocation/:bloodType/:bloodRH/:size/:user', asyncHandler(async (req,res,next) =>{
+/**
+ * Creates a new blood bag
+ */
+app.post('/blood/create/:bagId/:bagOriginID/:bagLocation/:bloodType/:bloodRH/:size/:user', asyncHandler(async (req, res, next) => {
     const received = req.params
-	 const origin = req.get('origin');
-	 res.header('Access-Control-Allow-Origin', origin);
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-    if(received.bagId == "" || received.bagOriginID == "" || received.bagLocation == "" || received.bloodType == "" || received.bloodRH == "" || received.size == "" || received.user == ""){
+    if (received.bagId == "" || received.bagOriginID == "" || received.bagLocation == "" || received.bloodType == "" || received.bloodRH == "" || received.size == "" || received.user == "") {
         res.sendStatus(500)
     }
-    const result = await invoke.createBloodBag(received.bagId,received.bagOriginID,received.bagLocation,received.bloodType,received.bloodRH,received.size,received.user)
+    const result = await invoke.createBloodBag(received.bagId, received.bagOriginID, received.bagLocation, received.bloodType, received.bloodRH, received.size, received.user)
     console.log(result)
     res.sendStatus(200)
- }))
+}))
 
- /**
-  * Reads blood bags
-  */
- app.get('/blood/read/:bagId/:user',asyncHandler(async (req,res,next) => {
-     const received = req.params
-	 const origin = req.get('origin');
-	 res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-     if(received.bagId == "" || received.user == "") {
-        res.sendStatus(500)
-     }
-     const result = await query.readBloodBag(received.bagId,received.user)
-     res.send(result)
- }))
-
- /**
-  * Gets transaction history for a Blood Bag
-  */
- app.get('/blood/history/:bagId/:user',asyncHandler(async (req,res,next) => {
+/**
+ * Reads blood bags
+ */
+app.get('/blood/read/:bagId/:user', asyncHandler(async (req, res, next) => {
     const received = req.params
-	 const origin = req.get('origin');
-	 res.header('Access-Control-Allow-Origin', origin);
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-    if(received.bagId == "" || received.user == "") {
-       res.sendStatus(500)
+    if (received.bagId == "" || received.user == "") {
+        res.sendStatus(500)
     }
-    const result = await query.getHistoryForBloodBag(received.bagId,received.user)
+    const result = await query.readBloodBag(received.bagId, received.user)
+    res.send(result)
+}))
+
+/**
+ * Gets transaction history for a Blood Bag
+ */
+app.get('/blood/history/:bagId/:user', asyncHandler(async (req, res, next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.bagId == "" || received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await query.getHistoryForBloodBag(received.bagId, received.user)
     res.send(result)
 }))
 
 /**
  * Moves a bag to a new location
  */
-app.post('/blood/move/:bagId/:location/:user',asyncHandler(async (req,res,next) => {
+app.post('/blood/move/:bagId/:location/:user', asyncHandler(async (req, res, next) => {
     const received = req.params
-	const origin = req.get('origin');
-	res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-    if(received.bagId == "" || received.location == "" || received.user == ""){
-        res.sendStatus(500)
-    }
-    const result = await invoke.moveBagToLocation(received.bagId,received.location,received.user)
-    res.send(result)
-}))
-
-app.post('/blood/assign/:bagId/:recipient/:destination/:user',asyncHandler(async (req,res,next) => {
-    const received = req.params
-	const origin = req.get('origin');
+    const origin = req.get('origin');
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
-    if(received.bagId == "" || received.recipient == "" || received.destination == "" || received.user == ""){
+    if (received.bagId == "" || received.location == "" || received.user == "") {
         res.sendStatus(500)
     }
-    const result = await invoke.assignBloodBagReceiver(received.bagId,received.recipient,received.destination,received.user)
+    const result = await invoke.moveBagToLocation(received.bagId, received.location, received.user)
+    res.send(result)
+}))
+
+app.post('/blood/assign/:bagId/:recipient/:destination/:user', asyncHandler(async (req, res, next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.bagId == "" || received.recipient == "" || received.destination == "" || received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await invoke.assignBloodBagReceiver(received.bagId, received.recipient, received.destination, received.user)
+    res.send(result)
+}))
+
+app.post('/rewards/create/:userId/:user', asyncHandler(async (req, res, next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.userId == "" || received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await invoke.createWallet(received.userId, received.user)
+    res.send(result)
+}))
+
+app.get('/rewards/history/:userId/:user', asyncHandler(async (req, res, next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.userId == "" || received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await query.getRewardsHistory(received.userId,received.user)
+    res.send(result)
+}))
+
+app.get('/rewards/read/:userId/:user', asyncHandler(async (req,res,next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.userId == "" || received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await query.readWallet(received.userId,received.user)
+    res.send(result)
+}))
+
+app.post('/rewards/receive/:userId/:tokens/:user', asyncHandler(async (req,res,next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.userId == "" || received.tokens ||received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await invoke.receiveTokens(received.userId,received.tokens,received.user)
+    res.send(result)
+}))
+
+app.post('/rewards/spend/:userId/:amount/:user', asyncHandler(async (req,res,next) => {
+    const received = req.params
+    const origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, Pragma');
+    if (received.userId == "" || received.amount ||received.user == "") {
+        res.sendStatus(500)
+    }
+    const result = await invoke.spendTokens(received.userId, received.amount, received.user)
     res.send(result)
 }))
 
 
-app.listen(port,()=>console.log(`Blockchain API listening on port ${port}!`))
+app.listen(port, () => console.log(`Blockchain API listening on port ${port}!`))
